55fb0022ff99cd081fac538626657361912dca26
Immanuel Sitepu <immanuel.sitepu001@binus.ac.id>
2023-06-30 14:38:51 +0700

    Initial Commit

---
+++ BWeather/BWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BWeatherApp.swift
+//  BWeather
+//
+//  Created by Immanuel Sitepu on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
